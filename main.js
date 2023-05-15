4c5b0059ca21242e76e519ee5042c007642c80a7
igor <igor.kolivushka47@gmail.com>
2023-05-15 07:51:21 +0300

    first

---
+++ main.js
@@ -0,0 +1,32 @@
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последний просморенных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
